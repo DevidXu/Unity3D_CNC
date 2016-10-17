72c1eb4df1782df92c604471039b2fa210758a61
DevidXu <869378898@qq.com>
2016-10-17 09:51:06 +0800

    Data process in C# in Unity3D
    Postgre_SQL provide the method to read data from PostGre database. Pay
    attention that the function has been made to be real time since I use
    trigger in PgAdmin to build a one-line table used for data transmission,
    where the data size is small so that you don't have to worry about
    delay. Lathe is just used to access data from Postgre_SQL.

---
+++ Unity3D_Data_From Postgre/Lathe.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CTRotationType { Uniform, AccelerateUniformly }
+public class Lathe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        cir = GameObject.FindWithTag("Cube_Cir");
+        cut = GameObject.FindWithTag("Cube_Cut");
+        cut_ext = GameObject.FindWithTag("Cube_Cut_Ext");
+        cut_ext2 = GameObject.FindWithTag("Cube_Cut_Ext2");
+        dome1 = GameObject.FindWithTag("Cube_Dome1");
+        dome1_dc = GameObject.FindWithTag("Cube_Dome1_DC");
+        fte = GameObject.FindWithTag("Cube_Fte");
+
+	}
+
+    GameObject cir, cut, cut_ext, cut_ext2, dome1, dome1_dc, fte;
+
+	// Update is called once per frame
+	void Update () {
+        if (!convert)
+        {
+            speed1 = (float)this.GetComponent<PostGre_SQL>().x_axis * 100;
+            speed2 = (float)this.GetComponent<PostGre_SQL>().y_axis * 100;
+        }
+        cir.GetComponent<Axis_Rotate_Cir>().RotateTo(speed1 / 2, 2, 1.0f);
+        cut.GetComponent<Axis_Rotate_Cut>().RotateTo(speed1, 2, 1.0f);
+        cut_ext.GetComponent<Axis_Rotate_Cut_Ext>().RotateTo(speed2, 0, 1.0f);
+        cut_ext2.GetComponent<Axis_Rotate_Cut_Ext2>().RotateTo(speed2, 2, 1.0f);
+        dome1.GetComponent<Axis_Rotate_Dome1>().RotateTo(speed1, 0, 1.0f);
+        dome1_dc.GetComponent<Axis_Rotate_Dome1_DC>().RotateTo(speed2, 2, 1.0f);
+        fte.GetComponent<Axis_Rotate_Fte>().RotateTo(speed1, 2, 1.0f);
+        print(speed1);
+        print(speed2);
+	}
+
+    public string speed1_text = "Speed： ";
+    public string speed1_input = "Input Speed  ";
+    public string speed2_text = "Speed： ";
+    public string speed2_input = "Input Speed  ";
+    float speed1 = 100.0f, speed2 = 100.0f;
+    bool convert = false;
+
+    void OnGUI()
+    {
+        GUI.TextField(new Rect(0, 10, 70, 20), speed1_text, 10);
+        GUI.TextField(new Rect(0, 30, 70, 40), speed2_text, 10);
+        speed1_input = GUI.TextField(new Rect(70, 10, 120, 20), speed1_input, 10);
+        speed2_input = GUI.TextField(new Rect(70, 30, 120, 40), speed2_input, 10);
+
+        if (float.TryParse(speed1_input, out speed1) && float.TryParse(speed2_input, out speed2))
+            convert = true;
+        else convert = false;
+        if (speed1 > 1000.0f) speed1 = 1000.0f;
+        if (speed2 > 1000.0f) speed2 = 1000.0f;
+    }
+
+}
---
+++ Unity3D_Data_From Postgre/PostGre_SQL.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using Npgsql;
+using NpgsqlTypes;
+using Mono.Security;
+using System.Data;
+using System.Data.SqlClient;
+
+public class PostGre_SQL : MonoBehaviour {
+    NpgsqlConnection conn;
+    NpgsqlCommand command;
+    NpgsqlDataReader dr1, dr2;
+    public const int MAX_LEN = 10000;
+
+    public double x_axis, y_axis;
+    int k = 0, i = 0;
+    int count_time = 0;
+    string[] x = new string[MAX_LEN];
+    string[] y = new string[MAX_LEN];
+    string x_axis_sql = "select cx_axis from data_update";
+    string y_axis_sql = "select cy_axis from data_update";
+
+    void Start(){
+        print(TestConnection());
+    }
+
+    void Update(){
+        command = new NpgsqlCommand(x_axis_sql, conn);
+        dr1 = command.ExecuteReader();
+        k = 0;
+        while (dr1.Read() && k < MAX_LEN) x[k++] = dr1[0].ToString();
+
+        command = new NpgsqlCommand(y_axis_sql, conn);
+        dr2 = command.ExecuteReader();
+        k = 0;
+        dr2 = command.ExecuteReader();
+        while (dr2.Read() && k < MAX_LEN) y[k++] = dr2[0].ToString();
+
+        count_time = count_time + (int)(Time.deltaTime * 1000);
+        if (count_time > 1000)
+        {
+            i++;
+            count_time = count_time % 1000;
+        }
+        
+        if (i >= 1000) i -= 1000;
+        i = i % MAX_LEN;
+
+        double.TryParse(x[0], out x_axis);
+        double.TryParse(y[0], out y_axis);
+    }
+
+    private string TestConnection()
+    {
+        string str = "Server=127.0.0.1;Port=5432;UserId=postgres;Password=siemens;Database=CNCXYZ;";
+        string strMessage = string.Empty;
+        try
+        {
+            conn = new NpgsqlConnection(str);
+            conn.Open();
+            strMessage = "Success";
+        }
+        catch
+        {
+            strMessage = "Failure";
+        }
+        return strMessage;
+    }
+
+    ~PostGre_SQL() { conn.Close(); }
+}
